f3bd322a4dd674d79398eb3ee752271d0df5ff5b
Michael Haar <michael.haar92@gmail.com>
2024-09-13 17:26:55 +0200

    Update scatter-plot.tsx

--- components/ui/scatter-plots/scatter-plot.tsx
+++ components/ui/scatter-plots/scatter-plot.tsx
@@ -1,26 +1,13 @@
 'use client';
 
 import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Scatter, XAxis, YAxis, CartesianGrid, Line, ComposedChart } from 'recharts';
-import { ChartConfig, ChartContainer } from '../chart';
+import { Scatter, XAxis, YAxis, CartesianGrid, Dot, ScatterChart } from 'recharts';
+import { ChartContainer } from '../chart';
 
-const chartConfig = {
-  count: {
-    label: 'Anzahl',
-    color: 'hsl(var(--chart-1))',
-  },
-} satisfies ChartConfig;
-
-type DataPoint =
-  | {
-      x: number | null;
-      y: number | null;
-    }
-  | {
-      x: number;
-      yLine: number;
-    };
+type DataPoint = {
+  x: number | null;
+  y: number | null;
+};
 
 export type ScatterData = DataPoint[];
 
@@ -30,45 +17,50 @@ type Props = {
 
 export default function ScatterPlot({ data }: Props) {
   return (
-    <Card className="w-full">
-      <CardHeader>
-        <CardTitle>Preis vs. Größe</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-          <ComposedChart
-            data={data}
-            margin={{
-              top: 20,
-              right: 20,
-              bottom: 20,
-              left: 20,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" className="stroke-muted" />
-            <XAxis
-              type="number"
-              dataKey="x"
-              name="X Axis"
-              unit="m²"
-              className="text-sm"
-              domain={[0, 200]}
-              allowDataOverflow
-            />
-            <YAxis
-              type="number"
-              dataKey="y"
-              name="Y Axis"
-              unit="k€"
-              className="text-sm"
-              domain={[0, 1000]}
-              allowDataOverflow
-            />
-            <Scatter name="Data Points" dataKey="y" fill="hsl(var(--primary))" isAnimationActive={false} />
-            <Line type="monotone" dataKey="yLine" stroke="hsl(var(--primary))" strokeDasharray="5 5" />
-          </ComposedChart>
-        </ChartContainer>
-      </CardContent>
-    </Card>
+    <ChartContainer config={{}}>
+      <ScatterChart
+        data={data}
+        margin={{
+          left: -20,
+          right: 20,
+          bottom: 20,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" className="stroke-muted" />
+        <XAxis
+          orientation="top"
+          type="number"
+          dataKey="x"
+          name="X Axis"
+          unit="m²"
+          className="text-[8px]"
+          domain={[0, 200]}
+          allowDataOverflow
+          axisLine={false}
+        />
+        <YAxis
+          reversed
+          type="number"
+          dataKey="y"
+          name="Y Axis"
+          unit="k"
+          className="text-[8px]"
+          domain={[0, 1000]}
+          allowDataOverflow
+          axisLine={false}
+        />
+        <Scatter
+          name="Data Points"
+          dataKey="y"
+          fill="hsl(var(--primary))"
+          isAnimationActive={false}
+          shape={<CustomDot />}
+        />
+      </ScatterChart>
+    </ChartContainer>
   );
+}
+
+function CustomDot({ cx, cy }: { cx?: number; cy?: number }) {
+  return <Dot cx={cx} cy={cy} fill="hsl(var(--primary))" r={3} style={{ opacity: 0.4 }} />;
 }
