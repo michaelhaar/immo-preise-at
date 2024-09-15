d42c071a3a6430fde5a89a11e5c42e2777ac8a62
Michael Haar <michael.haar92@gmail.com>
2024-09-15 14:50:20 +0200

    improve desktop experience

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { KeyPerformanceIndicators } from './key-performance-indicators';
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex w-full flex-col items-center gap-8">
+      <main className="row-start-2 flex w-full max-w-3xl flex-col items-center gap-8">
         <h1 className="text-2xl font-bold sm:text-3xl">Preise Eigentumswohnungen Graz</h1>
         <Suspense>
           <div className="flex gap-4">
