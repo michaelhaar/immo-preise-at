2b99b95db87008c0ec032d8021b9707d53e5ce52
Michael Haar <michael.haar92@gmail.com>
2024-09-19 10:23:42 +0200

    fix import

--- components/ui/filters/filter-regions.tsx
+++ components/ui/filters/filter-regions.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRegionsSearchParamsState } from '@/hooks/use-search-params-state';
+import { useRegionsSearchParamsState } from '@/hooks/use-regions-search-params-state';
 import { allDistricts, allPostalCodes } from '@/lib/postal-codes-by-district';
 import { FancyMultiSelect } from '../fancy-multi-select';
 
