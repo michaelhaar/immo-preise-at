01dc6a370e6028a47ea6c03c0206c83e4eb2d606
Michael Haar <michael.haar92@gmail.com>
2024-09-22 13:56:49 +0200

    implement request caching

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -2,12 +2,35 @@ import { createTRPCContext } from '@/lib/trpc/init';
 import { appRouter } from '@/trpc-routers/app/app';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 
+const ONE_HOUR_IN_SECONDS = 60 * 60;
+
 const handler = (req: Request) =>
   fetchRequestHandler({
     endpoint: '/api/trpc',
     req,
     router: appRouter,
     createContext: createTRPCContext,
+    responseMeta(_opts) {
+      return {
+        headers: new Headers([
+          ['cache-control', `s-maxage=${secondsUntilNext5AmUtc()}, stale-while-revalidate=${ONE_HOUR_IN_SECONDS}`],
+        ]),
+      };
+    },
   });
 
 export { handler as GET, handler as POST };
+
+function secondsUntilNext5AmUtc() {
+  const now = new Date();
+  const target = new Date(now);
+
+  target.setUTCHours(5, 0, 0, 0);
+
+  if (now.getUTCHours() >= 5) {
+    target.setUTCDate(target.getUTCDate() + 1);
+  }
+
+  const diffSeconds = Math.floor((target.getTime() - now.getTime()) / 1000);
+  return diffSeconds;
+}
