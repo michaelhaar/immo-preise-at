58cd9c1026c8666e2c79450afbe12ada4dc6d63b
Michael Haar <michael.haar92@gmail.com>
2024-09-22 20:15:51 +0200

    implement request caching (#15)
    * implement request caching
    
    * debug statement
    
    * remove unnecessary stuff
    
    * remove leftovers

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -8,6 +8,25 @@ const handler = (req: Request) =>
     req,
     router: appRouter,
     createContext: createTRPCContext,
+    responseMeta(_opts) {
+      return {
+        headers: new Headers([['cache-control', `s-maxage=${secondsUntilNext5AmUtc()}, must-revalidate`]]),
+      };
+    },
   });
 
 export { handler as GET, handler as POST };
+
+function secondsUntilNext5AmUtc() {
+  const now = new Date();
+  const target = new Date(now);
+
+  target.setUTCHours(5, 0, 0, 0);
+
+  if (now.getUTCHours() >= 5) {
+    target.setUTCDate(target.getUTCDate() + 1);
+  }
+
+  const diffSeconds = Math.floor((target.getTime() - now.getTime()) / 1000);
+  return diffSeconds;
+}
