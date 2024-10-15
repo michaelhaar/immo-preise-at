8a66b9d353026b1a3eecfc241a2bfeaa42141811
Michael Haar <michael.haar92@gmail.com>
2024-10-15 16:52:50 +0200

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const dateRangeOptions = ['7T', '1M', '3M', '6M', '1J'] as const;
 export type DateRangeOption = (typeof dateRangeOptions)[number];
 
-export const defaultDateRangeOption = dateRangeOptions[1];
+export const defaultDateRangeOption = dateRangeOptions[4];
 
 export const supportedHistogramColumnNames = [
   'livingArea',
