d0591fe9fe35bf78b72c438fc622e8d8fe692382
Michael Haar <michael.haar92@gmail.com>
2024-11-29 21:05:19 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { FilterRegions } from '@/components/ui/filters/filter-regions';
 import { Filters } from '@/components/ui/filters/filters';
 import { RentBuySection } from '@/components/ui/rent-buy-section';
 import { Viewport } from 'next';
-import Link from 'next/link';
 import { Suspense } from 'react';
 
 export const viewport: Viewport = {
@@ -13,7 +12,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-items-center gap-16 p-4 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex w-full max-w-3xl flex-col gap-8">
-        <div>
+        {/* <div>
           <div className="font-bold">Woher kommen die angezeigten Daten?</div>
           <p className="mt-1">
             <Link className="underline" href="https://www.willhaben.at/" target="_blank">
@@ -30,10 +29,10 @@ export default function Home() {
             werden Preis, m2 und PLZ von Immobilien-Inseraten anonymisiert extrahiert und auf dieser Seite aufbereitet,
             um dir einen Überblick über den Immobilienmarkt zu geben.
           </p>
-        </div>
+        </div> */}
         <Suspense>
           <div>
-            <div className="text-xl font-bold">Welche Region interessiert dich?</div>
+            <div className="mt-4 text-xl font-bold">Welche Region interessiert dich?</div>
             <div className="mt-4 flex gap-4">
               <FilterRegions />
             </div>
